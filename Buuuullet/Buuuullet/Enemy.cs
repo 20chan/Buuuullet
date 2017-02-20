a58fed0e01900b59c637e34f4ffb3ed7d8cf6fa3
phillyai <sonagi.philly@gmail.com>
2017-02-21 02:12:21 +0900

    부끄럽지만 빈 클래스도 추가했사옵니다 호호호

---
+++ Buuuullet/Buuuullet/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Buuuullet
+{
+    public class Enemy : GameObject
+    {
+        public Enemy(Game game, SpriteBatch sb) : base(game, sb)
+        {
+
+        }
+    }
+}
---
+++ Buuuullet/Buuuullet/World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Buuuullet
+{
+    public class World : DrawableGameComponent
+    {
+        public Player Player;
+        
+        public World(Game game) : base(game)
+        {
+
+        }
+    }
+}
